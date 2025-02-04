7f5445a64aed92afd9c32c754d4b6c2ed020379a
Shubham Dhanvate <shubhamssd10@gmail.com>
2025-02-04 21:05:28 +0530

    Initial Set Up

---
+++ game-store-app/src/main/java/com/gameStore/game_store_app/GameStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.gameStore.game_store_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreAppApplication.class, args);
+	}
+
+}
---
+++ game-store-app/src/test/java/com/gameStore/game_store_app/GameStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameStore.game_store_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
