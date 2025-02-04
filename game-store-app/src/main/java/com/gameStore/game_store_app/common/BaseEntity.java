ac111267bd86b2c5d5a92d882dceaa743d46c7e4
Shubham Dhanvate <shubhamssd10@gmail.com>
2025-02-04 21:17:44 +0530

    Implement the BaseEntity class

---
+++ game-store-app/src/main/java/com/gameStore/game_store_app/common/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.gameStore.game_store_app.common;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+}
